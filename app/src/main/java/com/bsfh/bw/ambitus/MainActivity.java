19575ef3f532bf5786abd593c8c98e0b325af4d0
bethw <beth.williams@bluewin.ch>
2022-11-28 15:36:26 +0100

    add frontend implementation for main activity

---
+++ app/src/main/java/com/bsfh/bw/ambitus/MainActivity.java
@@ -0,0 +1,27 @@
+package com.bsfh.bw.ambitus;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
