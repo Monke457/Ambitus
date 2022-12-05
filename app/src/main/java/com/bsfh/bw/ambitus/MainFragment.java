c4e9fa9f554dd5d10499e1e50157fcddbf52ef5c
Monke457 <74180551+Monke457@users.noreply.github.com>
2022-12-05 10:56:59 +0100

    Navigation functionality (#4)
    * add navigation functionality
    
    * optimise imports
    
    Co-authored-by: bethw <beth.williams@bluewin.ch>

---
+++ app/src/main/java/com/bsfh/bw/ambitus/MainFragment.java
@@ -0,0 +1,30 @@
+package com.bsfh.bw.ambitus;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        FloatingActionButton settingsButton = view.findViewById(R.id.settings_button);
+        settingsButton.setOnClickListener(v -> Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_settingsFragment));
+    }
+}
