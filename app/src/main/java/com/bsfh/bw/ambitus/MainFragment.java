1907d6a4070e46bce3ec76133a4ddb2779c02016
Monke457 <74180551+Monke457@users.noreply.github.com>
2022-12-05 16:22:00 +0100

    Add notification push functionality (#8)
    * check sensor values against normal range
    
    * add notification functionality
    add service SensorService
    
    Co-authored-by: bethw <beth.williams@bluewin.ch>

--- app/src/main/java/com/bsfh/bw/ambitus/MainFragment.java
+++ app/src/main/java/com/bsfh/bw/ambitus/MainFragment.java
@@ -119,7 +119,7 @@ public class MainFragment extends Fragment implements SensorEventListener {
         String text = values[0] + "°C";
         int min = prefs.getInt("battery-min", 5);
         int max = prefs.getInt("battery-max", 50);
-        text += getSuffix(min, max, values[0]);
+        text += validateValue(min, max, values[0]);
         tempValueView.setText(text);
     }
 
@@ -127,7 +127,7 @@ public class MainFragment extends Fragment implements SensorEventListener {
         String text = values[0] + "°C";
         int min = prefs.getInt("temp-min", -5);
         int max = prefs.getInt("temp-max", 30);
-        text += getSuffix(min, max, values[0]);
+        text += validateValue(min, max, values[0]);
         ambientTempValueView.setText(text);
     }
 
@@ -135,7 +135,7 @@ public class MainFragment extends Fragment implements SensorEventListener {
         String text = values[0] + "%";
         int min = prefs.getInt("humidity-min", 10);
         int max = prefs.getInt("humidity-max", 80);
-        text += getSuffix(min, max, values[0]);
+        text += validateValue(min, max, values[0]);
         humidityValueView.setText(text);
     }
 
@@ -143,7 +143,7 @@ public class MainFragment extends Fragment implements SensorEventListener {
         String text = values[0] + " hPa";
         int min = prefs.getInt("pressure-min", 550);
         int max = prefs.getInt("pressure-max", 1240);
-        text += getSuffix(min, max, values[0]);
+        text += validateValue(min, max, values[0]);
         pressureValueView.setText(text);
     }
 
@@ -152,7 +152,7 @@ public class MainFragment extends Fragment implements SensorEventListener {
         magnetValueView.setText(text);
     }
 
-    private String getSuffix(int min, int max, float value) {
+    private String validateValue(int min, int max, float value) {
         if (value < min) {
             return " [TOO LOW!]";
         } else if (value > max) {
