300feea171269764c5c8f74b790fac98d94b2904
Monke457 <74180551+Monke457@users.noreply.github.com>
2022-12-05 13:14:38 +0100

    add sensor functionality (#5)
    Co-authored-by: bethw <beth.williams@bluewin.ch>

--- app/src/main/java/com/bsfh/bw/ambitus/MainActivity.java
+++ app/src/main/java/com/bsfh/bw/ambitus/MainActivity.java
@@ -1,28 +1,19 @@
 package com.bsfh.bw.ambitus;
 
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
     }
 }
--- app/src/main/java/com/bsfh/bw/ambitus/MainFragment.java
+++ app/src/main/java/com/bsfh/bw/ambitus/MainFragment.java
@@ -1,9 +1,18 @@
 package com.bsfh.bw.ambitus;
 
+import static android.content.Context.SENSOR_SERVICE;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.hardware.TriggerEvent;
+import android.hardware.TriggerEventListener;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,7 +21,22 @@ import androidx.navigation.Navigation;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainFragment extends Fragment {
+import java.util.Arrays;
+
+public class MainFragment extends Fragment implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor temperatureSensor;
+    private Sensor ambientTemperatureSensor;
+    private Sensor humiditySensor;
+    private Sensor pressureSensor;
+    private Sensor magnetSensor;
+
+    private TextView tempValueView;
+    private TextView ambientTempValueView;
+    private TextView humidityValueView;
+    private TextView pressureValueView;
+    private TextView magnetValueView;
 
     @Nullable
     @Override
@@ -24,7 +48,95 @@ public class MainFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        tempValueView = view.findViewById(R.id.battery_value);
+        ambientTempValueView = view.findViewById(R.id.temp_value);
+        humidityValueView = view.findViewById(R.id.humidity_value);
+        pressureValueView = view.findViewById(R.id.pressure_value);
+        magnetValueView = view.findViewById(R.id.magnet_value);
+
+        if (this.getActivity() != null) {
+            sensorManager = (SensorManager) this.getActivity().getSystemService(SENSOR_SERVICE);
+            temperatureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_TEMPERATURE);
+            ambientTemperatureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+            humiditySensor = sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+            pressureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+            magnetSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+            if (temperatureSensor == null) tempValueView.setText(R.string.sensor_unavailable);
+            if (ambientTemperatureSensor == null) ambientTempValueView.setText(R.string.sensor_unavailable);
+            if (humiditySensor == null) humidityValueView.setText(R.string.sensor_unavailable);
+            if (pressureSensor == null) pressureValueView.setText(R.string.sensor_unavailable);
+            if (magnetSensor == null) magnetValueView.setText(R.string.sensor_unavailable);
+        }
+
         FloatingActionButton settingsButton = view.findViewById(R.id.settings_button);
         settingsButton.setOnClickListener(v -> Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_settingsFragment));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, temperatureSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, ambientTemperatureSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, humiditySensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, pressureSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, magnetSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        switch (event.sensor.getType()) {
+            case Sensor.TYPE_TEMPERATURE:
+                updateTempValue(event.values);
+                break;
+            case Sensor.TYPE_AMBIENT_TEMPERATURE:
+                updateAmbientTempValue(event.values);
+                break;
+            case Sensor.TYPE_RELATIVE_HUMIDITY:
+                updateHumidityValue(event.values);
+                break;
+            case Sensor.TYPE_PRESSURE:
+                updatePressureValue(event.values);
+                break;
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                updateMagnetValue(event.values);
+                break;
+            default: break;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    private void updateTempValue(float[] values) {
+        String text = values[0] + "°C";
+        tempValueView.setText(text);
+    }
+
+    private void updateAmbientTempValue(float[] values) {
+        String text = values[0] + "°C";
+        ambientTempValueView.setText(text);
+    }
+
+    private void updateHumidityValue(float[] values) {
+        String text = values[0] + "%";
+        humidityValueView.setText(text);
+    }
+
+    private void updatePressureValue(float[] values) {
+        String text = values[0] + " hPa";
+        pressureValueView.setText(text);
+    }
+
+    private void updateMagnetValue(float[] values) {
+        String text = values[0] + " μT  " + values[1] + " μT  " + values[2] + " μT";
+        magnetValueView.setText(text);
     }
 }
